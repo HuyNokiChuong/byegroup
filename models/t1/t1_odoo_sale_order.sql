4d14349771f4a28105c3a8f6736dda4f8a50a327
bye lun <odoo@byelun.com>
2025-04-17 09:12:53 +0000

    a

--- models/t1/t1_odoo_sale_order.sql
+++ models/t1/t1_odoo_sale_order.sql
@@ -19,8 +19,9 @@ SELECT
   IFNULL(CAST(o.money_collection AS float64),0) as money_collection, -- Tiền COD
   CASE 
     WHEN IFNULL(CAST(o.deposit_amount AS float64),0)  > 0 and IFNULL(CAST(o.deposit_amount AS float64),0)  < IFNULL(CAST(o.money_collection AS float64),0) then 'Đã cọc 1 phần, chưa thanh toán hết'
-      WHEN IFNULL(CAST(o.deposit_amount AS float64),0)  > 0 and IFNULL(CAST(o.money_collection AS float64),0) = 0 THEN 'Đã thanh toán đủ'
+    WHEN IFNULL(CAST(o.deposit_amount AS float64),0)  > 0 and IFNULL(CAST(o.money_collection AS float64),0) = 0 THEN 'Đã thanh toán đủ'
     WHEN IFNULL(CAST(o.deposit_amount AS float64),0)  = 0 and IFNULL(CAST(o.money_collection AS float64),0) <> 0 THEN 'COD'
+    WHEN IFNULL(CAST(o.deposit_amount AS float64),0)  = 0 and IFNULL(CAST(o.money_collection AS float64),0) = 0 THEN 'Đơn 0 đồng, và không cần thu tiền'
     WHEN o.name like '%PS%' THEN 'Đơn đẩy từ Pushsale qua'
   ELSE 'Khác' End as payment_type,
   o.amount_total+cast(total_discount_amount as float64) - CAST(amount_tax as float64) as total_amount,
