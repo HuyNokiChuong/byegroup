5525d3cb834f2e99831b2cbadd0890dc8e45ee4a
bye lun <odoo@byelun.com>
2025-04-19 15:51:39 +0000

    a

--- models/t1/t1_odoo_sale_order.sql
+++ models/t1/t1_odoo_sale_order.sql
@@ -17,6 +17,7 @@ SELECT
   o.shipping_provider,
   p.name as seller_employee_name,
   p1.name as marketing_employee_name,
+  lc.name as source_name_category,
   us.name as source_name,
   IFNULL(CAST(o.deposit_amount AS float64),0) as deposit_amount,--Tiền cọc
   IFNULL(CAST(o.money_collection AS float64),0) as money_collection, -- Tiền COD
@@ -46,6 +47,7 @@ FROM
  left join byebeo.res_users u1 on o.marketing_id = CAST(u1.id as string)
  left join byebeo.res_partner p1 on CAST(u1.partner_id as string) = CAST(p1.id as string)
  left join byebeo.utm_source us on o.source_id = CAST(us.id as string) and o.company_id = us.company_id
+ left join byebeo.crm_lead_channel lc on us.channel_id = lc.id
 --  where o.name = 'S00784'
 
 
--- models/t1/t1_odoo_sale_order_line.sql
+++ models/t1/t1_odoo_sale_order_line.sql
@@ -26,6 +26,7 @@ CASE o.delivery_status
   WHEN 'partial' then 'Đã giao 1 phần'
   WHEN 'full' then 'Đã giao hết'
 END as delivery_status,
+lc.name as source_name_category,
 us.name as source_name,
 ol.name as product_name, ---tên saaaản phẩm
 ol.product_uom, --Số lượng
@@ -49,4 +50,5 @@ ol.price_subtotal + price_tax as final_amount,
  left join byebeo.crm_team t on o.team_id = cast(t.id as string)
  left join byebeo.res_users u1 on o.marketing_id = CAST(u1.id as string)
  left join byebeo.utm_source us on o.source_id = CAST(us.id as string) and o.company_id = us.company_id
+ left join byebeo.crm_lead_channel lc on us.channel_id = lc.id
 --  where o.name = 'S00357'
